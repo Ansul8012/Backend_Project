7c4742d0c3ae7da59476c9f6530b12905967eeb4
Ansul <ansulsingh171@gmail.com>
2025-05-21 19:31:20 +0530

    done

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-
+import { getUsers,getUser } from '../controllers/user.controller.js';
 
 const userRouter = Router();
 
-// userRouter.get('/', getUsers);
+userRouter.get('/', getUsers);
 
-// userRouter.get('/:id', authorize, getUser);
+userRouter.get('/:id', getUser);
 
 userRouter.post('/', (req, res) => res.send({ title: 'CREATE new user' }));
 
