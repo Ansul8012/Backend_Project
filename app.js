feed2bc03cbcb7ae6eba5d63c0dffc04fe3fd6d9
Ansul <ansulsingh171@gmail.com>
2025-05-26 12:58:59 +0530

    done

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 import connectToDatabase from './database/mongodb.js'
 import arcjetMiddleware from './middlewares/arcjet.middleware.js';
+import workflowRouter from './routes/workflow.routes.js';
 
 const app = express();
 app.set('trust proxy', true);
@@ -20,6 +21,7 @@ app.use(cookieParser());
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+app.use('/api/v1/workflows', workflowRouter);
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Subscription Tracker API! lalala yoooo hie');
